af45a00ffb0750c7093ef802542d1e5dbc6ccaea
Josh N. Abbott <joshnabbott@gmail.com>
2011-05-12 14:05:16 -0600

    Fix paths to require lib files.

--- lib/casper_api.rb
+++ lib/casper_api.rb
@@ -1,22 +1,22 @@
 require 'httparty'
 require 'ostruct'
 require 'cgi'
-require 'lib/connection'
-require 'lib/casper'  
-require 'lib/list'
-require 'lib/list_item'
-require 'lib/buildings'
-require 'lib/building'
-require 'lib/categories'
-require 'lib/category'
-require 'lib/computer_groups'
-require 'lib/computer_group'
-require 'lib/computers'
-require 'lib/computer'
-require 'lib/departments'
-require 'lib/department'
-require 'lib/mobile_devices'
-require 'lib/mobile_device'
+require File.expand_path('../connection', __FILE__)
+require File.expand_path('../casper', __FILE__)
+require File.expand_path('../list', __FILE__)
+require File.expand_path('../list_item', __FILE__)
+require File.expand_path('../buildings', __FILE__)
+require File.expand_path('../building', __FILE__)
+require File.expand_path('../categories', __FILE__)
+require File.expand_path('../category', __FILE__)
+require File.expand_path('../computer_groups', __FILE__)
+require File.expand_path('../computer_group', __FILE__)
+require File.expand_path('../computers', __FILE__)
+require File.expand_path('../computer', __FILE__)
+require File.expand_path('../departments', __FILE__)
+require File.expand_path('../department', __FILE__)
+require File.expand_path('../mobile_devices', __FILE__)
+require File.expand_path('../mobile_device', __FILE__)
 
 module CasperApi
 end
